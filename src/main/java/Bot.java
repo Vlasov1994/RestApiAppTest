9a73fbf69ccc4ac393970ab8492887ccd956ef1a
vlasov.e <Kron1994!>
2021-07-10 01:27:04 +0300

    First Commit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,18 @@
+import org.apache.http.HttpException;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class Bot {
+    public static void main(String[] args) throws Exception {
+        Frontend front = new Frontend();
+
+        Server server = new Server(8080);
+        ServletContextHandler context  = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(front), "/api/webhook");
+
+        server.start();
+        server.join();
+    }
+}
