d2ff3b401ea16b0d577f391ccb50c83f1f58bdad
vlasov.e <Kron1994!>
2021-07-10 14:34:00 +0300

    Changing port to 5000

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -6,7 +6,7 @@ public class Bot {
     public static void main(String[] args) throws Exception {
         Frontend front = new Frontend();
 
-        Server server = new Server(8080);
+        Server server = new Server(5000);
         ServletContextHandler context  = new ServletContextHandler(ServletContextHandler.SESSIONS);
         server.setHandler(context);
         context.addServlet(new ServletHolder(front), "/api/webhook");
