a9ab4eb1eaaeb6cbae14d8c246408f9d6b32955e
vlasov.e <Kron1994!>
2021-07-10 20:12:35 +0300

    Add exception handler

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -2,16 +2,37 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Bot {
-    public static void main(String[] args) throws Exception {
-        Frontend front = new Frontend();
+    public static void main(String[] args) {
+        try {
+            Frontend front = new Frontend();
 
-        Server server = new Server(5000);
-        ServletContextHandler context  = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        server.setHandler(context);
-        context.addServlet(new ServletHolder(front), "/api/webhook");
+            Server server = new Server(5000);
+            ServletContextHandler context  = new ServletContextHandler(ServletContextHandler.SESSIONS);
+            server.setHandler(context);
+            context.addServlet(new ServletHolder(front), "/api/webhook");
 
-        server.start();
-        server.join();
+            server.start();
+            server.join();
+        } catch (Exception e) {
+            printExceptionInFile(e.getMessage());
+        }
+    }
+
+    public static void printExceptionInFile(String exception) {
+        try(FileWriter writer = new FileWriter("notes3.txt", false))
+        {
+            writer.write(exception);
+            writer.append('\n');
+            writer.append('E');
+
+            writer.flush();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
     }
 }
